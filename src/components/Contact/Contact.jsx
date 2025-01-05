ac3fb595e4a8568eb159b71d8183599d31c3844b
gracphan <gracphan@umich.edu>
2025-01-05 01:35:07 -0500

    Accomodated screen sizing

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,14 +10,20 @@ export const Contact = () => {
         <h2>Contact</h2>
         <p>Feel free to reach out!</p>
       </div>
-      <ul className={styles.links}>
-        <li className={styles.link}>
-          <a href="mailto:myemail@email.com">gracphan@umich.edu</a>
-        </li>
-        <li className={styles.link}>
-          <a href="https://www.linkedin.com/in/grace-phan/" target="_blank">linkedin.com/in/grace-phan/</a>
-        </li>
-      </ul>
+      <div className={styles.links}>
+        <a href="https://www.linkedin.com/in/grace-phan/" target="_blank" rel="noopener noreferrer">
+          <img
+            src={getImageUrl("contact/linkedin.png")}
+            className={styles.image}
+          />
+        </a>
+        <a href="mailto:gracephanpg@gmail.com">
+          <img
+            src={getImageUrl("contact/mail.png")}
+            className={styles.image}
+          />
+        </a>
+      </div>
     </footer>
   );
 };
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,16 +7,20 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Grace!</h1>
-        <p className={styles.description}>
-          I'm passionate about using technology to create innovative and meaningful solutions.
-        </p>
-        <p className={styles.checkWork}>Check out my work</p>
-        <img
-          src={getImageUrl("hero/down-arrow.png")}
-          alt="Down arrow"
-          className={styles.downArrow}
-        />
+        <div className={styles.introContainer}>
+          <h1 className={styles.title}>Hi, I'm Grace!</h1>
+          <p className={styles.description}>
+            I'm passionate about using technology to create innovative and meaningful solutions.
+          </p>
+        </div>
+        <div className={styles.checkOutContainer}>
+          <p className={styles.checkWork}>Check out my work</p>
+          <img
+            src={getImageUrl("hero/down-arrow.png")}
+            alt="Down arrow"
+            className={styles.downArrow}
+          />
+        </div>
       </div>
       <img
         src={getImageUrl("hero/IMG_1220.JPG")}
