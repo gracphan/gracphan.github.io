b998b0834bd0b3a73816f3ecf2c54e1b055d9561
gracphan <gracphan@umich.edu>
2025-11-06 19:10:39 -0500

    Initial commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.introContainer}>
+          <h1 className={styles.title}>Hi, I'm Grace!</h1>
+          <p className={styles.description}>
+            I'm a developer who's passionate about creating innovative and meaningful solutions.
+          </p>
+        </div>
+        <div className={styles.checkOutContainer}>
+          <p className={styles.checkWork}>Check out my work</p>
+          <img
+            src={getImageUrl("hero/down-arrow.png")}
+            alt="Down arrow"
+            className={styles.downArrow}
+          />
+        </div>
+      </div>
+      <img
+        src={getImageUrl("hero/IMG_1220.JPG")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+    </section>
+  );
+};
